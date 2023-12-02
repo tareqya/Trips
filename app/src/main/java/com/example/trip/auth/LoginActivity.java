edf0a025edeea091a56ec4e8304617fb10bdfebe
Tareq Yassin <tareq.ya95@gmail.com>
2023-12-02 14:58:19 +0200

    first commit

---
+++ app/src/main/java/com/example/trip/auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.trip.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.trip.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
