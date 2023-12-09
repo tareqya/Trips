1f913b21442922681d352734ae072529be65fdcc
Tareq Yassin <tareq.ya95@gmail.com>
2023-12-09 12:57:52 +0200

    finish design screens

---
+++ app/src/main/java/com/example/trip/home/QuizActivity.java
@@ -0,0 +1,16 @@
+package com.example.trip.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.trip.R;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
